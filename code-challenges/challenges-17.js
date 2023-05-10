2a0e86c7a7210516c7c409621d5b7f8a0379989a
Malek Al-Sheekh Hasan <malek.alsheekh01@gmail.com>
2023-05-10 11:40:27 +0000

    chall 17 is done.

--- code-challenges/challenges-17.js
+++ code-challenges/challenges-17.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // -------------------------------------------------------------------------------------------------------
 // Challenge 01:
@@ -13,41 +13,37 @@
 // Input: 50, 9
 // Output: [50, 41, 32, 23, 14, 5, -4, 5, 14, 23, 32, 41, 50]
 
-
 const recursionPattern = (int1, int2) => {
-    // write your code here
-    let arr1=[];
-    let maxNum=int1;
-    function myRec(int1,int2,arr)
+  // write your code here
+  let arr1 = [];
+  let maxNum = int1;
+  //Malek
+  function myRec(int1, int2, arr) {
+    arr.push(int1);
+    if (int2 > 0) {
+      if (int1 >= 0) {
+        myRec(int1 - int2, int2, arr);
+      } else if (arr[0] == int1) {
         {
-          arr.push(int1);
-          if(int2>0){
-            if(int1>=0)
-  {        myRec(int1-int2,int2,arr)
-  }          else if(arr[0]==int1){
-            {
-              return ;
-            }
-  
-          }
+          return;
         }
-          else{
-            if(maxNum>int1)
-            {
-               myRec(int1-int2,int2,arr)
-            }
-          }
-        }
-    myRec(int1,int2,arr1)
-    myRec(arr1[arr1.length-2],-int2,arr1)
-    return arr1;
-}
+      }
+    } else {
+      if (maxNum > int1) {
+        myRec(int1 - int2, int2, arr);
+      }
+    }
+  }
+  myRec(int1, int2, arr1);
+  myRec(arr1[arr1.length - 2], -int2, arr1);
+  return arr1;
+};
 // -------------------------------------------------------------------------------------------------------
 
 // -------------------------------------------------------------------------------------------------------
 // Challenge 02:
 // Required:
-// 
+//
 // Write a function that takes a string (HTML tag)
 // and extracts the link from the HTML tag
 //
@@ -56,53 +52,58 @@ const recursionPattern = (int1, int2) => {
 //
 // Note:
 //  Assume that links end with .com, .org or .net
-// 
+//
 
 const filterLinks = (str) => {
-    // write your code here
-    return str.split("").slice(str.indexOf('\/')+2,str.lastIndexOf('\"')).join("");
-}
+  // write your code here
+  return str
+    .split("")
+    .slice(str.indexOf("/") + 2, str.lastIndexOf('"'))
+    .join("");
+};
 // -------------------------------------------------------------------------------------------------------
 
 // -------------------------------------------------------------------------------------------------------
 // Challenge 03:
 // Required:
-// 
+//
 // A phrase is considered palindrome if, after converting all uppercase letters into lowercase letters
 // and removing all non-alphanumeric characters, it reads the same forward and backward.
 // Alphanumeric characters include letters and numbers.
 // Given a string s, return true if it is a palindrome, or false otherwise.
-// 
+//
 // Input: s = "A man, a plan, a canal: Panama"
 // Output: true
 // as you can see "amanaplanacanalpanama" is a palindrome.
 //
 
 const isPalindrome = (str) => {
-    // write your code here
-    let res=str.split("").map((ele)=>{
-        return ele.toLowerCase()
-        }).filter((ele)=>{
-        return ele >= "a" && ele<="z"?ele:'';
-        }).join("");
-        if(res.split("").reverse().join("")==res){
-        return true;
-        }
-        else{
-        return false;
-        
-        }
-}
+  // write your code here
+  let res = str
+    .split("")
+    .map((ele) => {
+      return ele.toLowerCase();
+    })
+    .filter((ele) => {
+      return ele >= "a" && ele <= "z" ? ele : "";
+    })
+    .join("");
+  if (res.split("").reverse().join("") == res) {
+    return true;
+  } else {
+    return false;
+  }
+};
 // -------------------------------------------------------------------------------------------------------
 
 // -------------------------------------------------------------------------------------------------------
 // Challenge 04:
 // Optional:
-// 
+//
 //  Write a function that takes 2 arguments, one is a string and the other is an array
 //  the function should return a boolean than indicates if both have the same pattern
 //
-//  EX: 
+//  EX:
 //  Input: "abba", ['cat', 'dog', 'dog', 'cat']
 //  output: true
 //
@@ -115,9 +116,8 @@ const isPalindrome = (str) => {
 //
 
 const samePattern = (str, arr) => {
-    // write your code here
-}
+  // write your code here
+};
 // -------------------------------------------------------------------------------------------------------
-
 
 module.exports = { recursionPattern, filterLinks, isPalindrome, samePattern };
